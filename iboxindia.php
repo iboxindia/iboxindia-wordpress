0e2f0b9b1916f155b0aec9c008f7a67b6cd7bf93
specky-geek <specky.geek@iboxindia.com>
2021-02-14 21:38:52 +0530

    updated version

--- iboxindia.php
+++ iboxindia.php
@@ -3,7 +3,7 @@
  * Plugin Name: Iboxindia
  * Plugin URI: https://wordpress.iboxindia.com/
  * Description: Plugin to configure sites for clients of IboxIndia. Kindly contact info@iboxindia.com for more information.
- * Version: 1.0.4
+ * Version: 1.0.5
  * Author: IboxIndia
  * Author URI: https://iboxindia.com
  * Text Domain: iboxindia 
